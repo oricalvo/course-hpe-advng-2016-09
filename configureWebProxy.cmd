ce01ad60b4b3c0989935bab0b5c227fd95688d1d
oricalvo <ori.calvo@gmail.com>
2016-09-06 09:35:08 +0300

    Adding ignore and configure files

---
+++ configureWebProxy.cmd
@@ -0,0 +1,4 @@
+CALL npm config set proxy http://web-proxy.isr.hp.com:8080/
+CALL npm config set https-proxy http://web-proxy.isr.hp.com:8080/
+CALL git config --global http.proxy http://web-proxy.isr.hp.com:8080/
+CALL git config --global https.proxy http://web-proxy.isr.hp.com:8080/
